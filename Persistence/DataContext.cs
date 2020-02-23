dcb4e28e9f783e85b9718b2fcd7414fdbe7011aa
Rhys <ewilliamr@hotmail.com>
2020-02-23 00:45:15 +0000

    End of section 14.

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -13,6 +13,8 @@ namespace Persistence
 
         public DbSet<Activity> Activities {get; set;}
 
+        public DbSet<UserActivity> UserActivities { get; set; }
+
         public DbSet<Value> Values {get; set;}
 
         protected override void OnModelCreating(ModelBuilder builder)
@@ -25,6 +27,16 @@ namespace Persistence
                     new Value{Id = 2, Name = "Value 102"},
                     new Value{Id = 3, Name = "Value 103"}
                 );
+
+                builder.Entity<UserActivity>().HasKey(k => new { k.AppUserId, k.ActivityId });
+                builder.Entity<UserActivity>()
+                    .HasOne(u => u.AppUser)
+                    .WithMany(a => a.UserActivities)
+                    .HasForeignKey(u => u.AppUserId);
+                builder.Entity<UserActivity>()
+                    .HasOne(a => a.Activity)
+                    .WithMany(a => a.UserActivities)
+                    .HasForeignKey(a => a.ActivityId);
         }
     }
 }
